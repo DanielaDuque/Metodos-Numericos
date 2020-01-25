0d15602399c47afc7c43339a36868e0210ff11e6
vbernalb <vbernalb@unal.edu.co>
2020-01-25 17:42:27 -0500

    Add files via upload

---
+++ Laboratorio 6/Punto 1/rks4.m
@@ -0,0 +1,24 @@
+function [T,Z] = rks4(F,a,b,Za,M)
+	#F es la funcion que esta como una cadena de caracteres
+	#a y b son los extremos derecho e izquierdo del intervalo
+	#Za es la condicion inicial
+	#M es el numero de pasos
+	#resultados
+	#T es el vector de los nodos
+	#Z=[x1(t)...x(n)] donde xk(t) es la aproximacion a la k esima variable dependiente
+
+	h=(b-a)/M;
+	T=zeros(1,M+1);
+	Z=zeros(M+1,length(Za));
+	T=a:h:b;
+	Z(1,:)=Za;
+
+	for j=1:M
+		k1=h*feval(F,T(j),Z(j,:));
+		k2=h*feval(F,T(j)+h/2,Z(j,:)+k1/2);
+		k3=h*feval(F,T(j)+h/2,Z(j,:)+k2/2);
+		k4=h*feval(F,T(j)+h,Z(j,:)+k3);
+		Z(j+1,:)=Z(j,:)+(k1+2*k2+2*k3+k4)/6;
+	endfor
+	
+endfunction
